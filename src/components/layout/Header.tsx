b416aa41094f83471641b16c04bdc8649546d218
sandeep <sandeepkumarpala07@gmail.com>
2026-09-25 10:35:57 +0530

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -28,7 +28,7 @@ function Header() {
           className="shrink-0"
         >
           <div className="font-serif text-xl font-bold tracking-tight text-[#570000] md:text-2xl">
-            Telangana Folk SongsSS
+            Telangana Folk Songs
           </div>
         </Link>
 
